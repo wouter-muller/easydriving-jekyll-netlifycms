9c053095aae7f428ff4edcd5494c1495f5ea4f62
Wouter Muller <wouter@woutermuller.net>
2019-04-29 13:59:44 +0200

    Add AJAX form

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,3 +5,23 @@ var app = new Vue({
         mobileMenuIsVisible: false
     }
 })
+
+let testForm = document.querySelector("#signup");
+
+testForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(testForm);
+  fetch(testForm.getAttribute('action'), {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    body: new URLSearchParams(formData).toString()
+  })
+  .then(res => {
+    if (res) {
+      console.log('true')
+    }
+  });
+});
